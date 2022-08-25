b02881db5c6fec3c7a3e492b0553a992b9869622
Sonola Moyo <sonolamoyosoluwalorun@gmail.com>
2022-08-25 01:59:31 +0100

    Metadata Model

---
+++ model/main.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const savedData = new mongoose.Schema({
+    fileName:{
+        type: String,
+        required: true,
+        index: true
+    },
+    originalName: {
+        type: String
+    },
+    size: {
+        type: Number
+    },
+    information: {
+        type: Object
+    }
+},{timestamps: true})
+
+const DataModel = mongoose.model('Metadata', savedData)
+
+export default {DataModel}
