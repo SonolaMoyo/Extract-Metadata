839f73b9f143fd76ced05e51a3719bbc525c3232
Sonola Moyo <sonolamoyosoluwalorun@gmail.com>
2022-08-27 00:42:49 +0100

    Understanding exiftool and how to use documentations, two routes now working

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -18,23 +18,32 @@ const createMetaData = async (req, res, next) => {
     const ep = new exiftool.ExiftoolProcess(distExiftool);
     ep.open()
       .then(() => ep.readMetadata(rs, ['-File:all']))
-      .then(async (result) => {
-        console.log('im here 1');
-        let metadata = new DataModel({
-          fileName: req.file.filename,
-          originalName: req.file.originalname,
-          size: req.file.size,
-          information: result.data[0],
-        });
-        metadata = await metadata.save();
-        return res.status(200).json({ metadata });
+      .then((res) => {
+        console.log(res)
       })
-      .then(console.log('here 3'))
-      //   .then(
-      //     () => ep.close(),
-      //     () => ep.close()
-      //   )
+      .then(
+        () => ep.close()
+      )
       .catch(console.error);
+      
+      return res.status(200).json({status: "success"});
+    // ep.open()
+    //   .then(() => ep.readMetadata(rs, ['-File:all']))
+    //   .then(async (result) => {
+    //     let metadata = new DataModel({
+    //       fileName: req.file.filename,
+    //       originalName: req.file.originalname,
+    //       size: req.file.size,
+    //       information: result.data,
+    //     });
+    //     metadata = await metadata.save();
+    //     return res.status(200).json({ metadata });
+    //   })
+    //   .then(
+    //     () => ep.close(),
+    //     () => ep.close()
+    //   )
+    //   .catch(console.error);
   } catch (error) {
     next(error);
   }
@@ -52,6 +61,30 @@ const getMetaData = async (req, res, next) => {
 
 const writeMetaData = async (req, res, next) => {
   try {
+    // if (!req.file) {
+    //   return res
+    //     .status(404)
+    //     .json({ status: 'failed', msg: 'file not found, upload one' });
+    // }
+    // const tempPath = path.join(
+    //   __dirname,
+    //   '/public/upload/file-exif-crimes_against_women_2001-2014.csv'
+    // );
+    // // const rs = fs.createReadStream(tempPath);
+    // const data = {
+    //   all: ''
+    // };
+    // const ep = new exiftool.ExiftoolProcess(/*distExiftool*/);
+    // ep.open()
+    //   .then(() => {
+    //     const editedData = ep.writeMetadata(tempPath, data, [
+    //       'overwrite_original',
+    //     ])
+    //     console.log(editedData);
+    //   })
+    //   .then(() => ep.close())
+    //   .catch(console.error);
+    // return res.status(200).json({ fileLocation: tempPath });
     if (!req.file) {
       return res
         .status(404)
@@ -60,23 +93,24 @@ const writeMetaData = async (req, res, next) => {
     const tempPath = path.join(__dirname, '/public/upload/', req.file.filename);
     console.log(tempPath);
     const rs = fs.createReadStream(tempPath);
-    const data = {
-      all: '',
-      comment: 'Exiftool rules!',
-      hello: 'world',
-      sonola: 'moyo',
-    };
-    const ep = new exiftool.ExiftoolProcess(distExiftool);
+    const ep = new exiftool.ExiftoolProcess();
     ep.open()
-      .then((rs) => {
-        ep.writeMetadata(rs, data, ['overwrite_original']);
-      })
-      .then(res.status(200).json({ fileLocation: tempPath }))
+      .then(() => ep.readMetadata(rs, ['n', '-File:all']))
+      .then((res) => 
+        ep.writeMetadata(
+          res.data[0].SourceFile,
+          {
+            "MWG:Megapixels": "another"
+          },
+          ['overwrite_original']
+        )
+      )
       .then(console.log, console.error)
       .then(() => ep.close())
       .catch(console.error);
+  return res.status(200).json({status: "success"});
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
