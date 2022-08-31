716d74cf8a8986eef9f1a785b4600f392c66ce7d
Sonola Moyo <sonolamoyosoluwalorun@gmail.com>
2022-08-31 12:09:36 +0100

    Update readFolder script

--- controllers/readFolder.js
+++ controllers/readFolder.js
@@ -8,6 +8,10 @@ const readFolder = async (directory) => {
   try {
     const tempPath = path.join(__dirname, '/public/upload/', directory);
     console.log(tempPath);
+    const rs = fs.createReadStream(tempPath);
+    if (!rs) {
+      return console.log('Unable to read Folder');
+    }
     const ep = new exiftool.ExiftoolProcess(distExiftool);
     ep.open()
       .then(() => ep.readMetadata(tempPath, ['-ext "*"', '-r', '-a', '-u', '-g1']))
